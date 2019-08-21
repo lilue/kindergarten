9cf0182122299547b3598fa1e300c5119767eb7a
lilue <aa_0933@qq.com>
2019-08-21 18:25:53 +0800

    debug

--- resources/views/payment/pay.blade.php
+++ resources/views/payment/pay.blade.php
@@ -4,7 +4,7 @@
 @section('script')
 @parent
 <script src="http://res.wx.qq.com/open/js/jweixin-1.2.0.js" type="text/javascript" charset="utf-8"></script>
-<script type="text/javascript">wx.config({{ $baseJson }});</script>
+<script type="text/javascript">wx.config(<?php echo $baseJson ?>);</script>
 <script type="text/javascript">
     function callpay()
     {
@@ -15,7 +15,6 @@
             signType: '<?= $config['signType'] ?>',
             paySign: '<?= $config['paySign'] ?>', // 支付签名
             success: function (res) {
-                // document.write(Object.entries(res));
                 if(res.errMsg == "chooseWXPay:ok" ) {
                     alert('成功了');
                 } else {
@@ -24,7 +23,6 @@
             },
             cancel: function (res) {
                 alert('取消支付');
-                // history.back();
             }
         });
     }
@@ -33,6 +31,6 @@
 
 @section('content')
 
-<button type="button" onclick="callpay()" class="btn btn-success btn-lg btn-block">Success</button>
+<button type="button" onclick="callpay()" class="btn btn-success btn-lg btn-block">确认支付</button>
 
 @stop 
