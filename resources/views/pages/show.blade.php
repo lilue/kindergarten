5373da08b661fce4f515917afab3493cd08068c2
lilue <aa_0933@qq.com>
2019-08-21 11:59:03 +0800

    debug

--- resources/views/pages/show.blade.php
+++ resources/views/pages/show.blade.php
@@ -9,16 +9,16 @@
   <tbody>
     <tr>
       <td>学生编号</th>
-      <td>{{ $date->XSBH }}</td>
+      <td>{{ $date->xsbh }}</td>
       <td>学生姓名</td>
-      <td>{{ $date->XSXM }}</td>
+      <td>{{ $date->xsxm }}</td>
     </tr>
     <tr>
-      @if(!empty($date->OPENID))
+      @if(!empty($date->openid))
       <td colspan="2">已关联微信</td>
       @endif
       <td>关联手机</th>
-      <td>{{ $date->SJHM }}</td>
+      <td>{{ $date->sjhm }}</td>
     </tr>
   </tbody>
 </table>
@@ -30,11 +30,11 @@
       <div class="card-body">
         <h5 class="card-title">收费单号：{{ $order->sfdh }}</h5>
         <p class="card-text">收费单金额：{{ $order->je }}</p>
-        <a href="{{ route('pages.custom', ['id' => $date->XSBH, 'dh' => $order->sfdh, 'je' => $order->je]) }}" class="btn btn-primary">前往缴费</a>
+        <a href="{{ route('pages.custom', ['id' => $date->xsbh, 'dh' => $order->sfdh, 'je' => $order->je]) }}" class="btn btn-primary">前往缴费</a>
       </div>
     </div>
   </div>
   @endforeach
 </div>
-<a class="btn btn-danger btn-lg btn-block my-2" href="{{ route('pages.cancel', ['id' => $date->XSBH]) }}" role="button">解除微信关联</a>
+<a class="btn btn-danger btn-lg btn-block my-2" href="{{ route('pages.cancel', ['id' => $date->xsbh]) }}" role="button">解除微信关联</a>
 @stop 
