5e01dac29ec084a488c50b3767572bf8c7adea0f
lilue <aa_0933@qq.com>
2019-08-21 12:04:42 +0800

    debug

--- resources/views/pages/root.blade.php
+++ resources/views/pages/root.blade.php
@@ -7,7 +7,7 @@
 
 <ul class="list-group">
 @foreach($students as $student)
-<a href="{{ route('pages.show', ['id' => $student->xsbh]) }}" class="list-group-item">{{ $student->XSXM }}</a>
+<a href="{{ route('pages.show', ['id' => $student->xsbh]) }}" class="list-group-item">{{ $student->xsbh }}</a>
 @endforeach
 </ul>
 <a class="btn btn-primary my-2" href="{{ route('pages.linked') }}" role="button">新增关联</a>
