0b6b74d677e184edb4864921ee93d98752093db7
lilue <aa_0933@qq.com>
2019-12-04 16:32:00 +0800

    update

--- app/Http/Controllers/TemplateController.php
+++ app/Http/Controllers/TemplateController.php
@@ -13,12 +13,13 @@ class TemplateController extends Controller
     {
         $app = EasyWeChat::officialAccount();
         $res = DB::table('table_xsxx')->where('xsbh', '2019080001')->first();
+        $first = "尊敬的" + $res->xsxm + "家长，您已成功缴费";
         $app->template_message->send([
             'touser'        =>  $res->openid,
             'template_id'   =>  'gnqSCMYGufoQI6RdPBWWqIqYY',
             'url'           =>  'http://www.baidu.com',
             'data'          =>  [
-                'first'         =>  '尊敬的' + $res->xsxm + '家长，您已成功缴费',
+                'first'         =>  $first,
                 'keyword1'      =>  $res->xsxm,
                 'keyword2'      =>  $res->bj,
                 'keyword3'      =>  $res->bj,
